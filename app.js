f82d719b8c7648de30bd56bc7b6d7574ee9a7cdb
Вадим Онегов <vadim.onegov@bars-open.ru>
2025-12-03 18:42:23 +0300

    3

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-import express from 'express';
-import appConfig from './config/main-config.js'
-import routeConfig from './config/route-config.js'
-import errorConfig from './config/error-config.js'
+(function() {
+  'use strict';
 
+  // Зависимости
+  const express = require('express');
 
-const app = express();
- 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+  const appConfig = require('./config/main-config.js');
+  const routeConfig = require('./config/route-config.js');
+  const errorConfig = require('./config/error-config.js');
 
-// *** config *** //
-appConfig.init(app, express);
-routeConfig.init(app);
-errorConfig.init(app);
-  
-export default app;
+  // *** express instance *** //
+  const app = express();
+
+  // *** config *** //
+  appConfig.init(app, express);
+  routeConfig.init(app);
+  errorConfig.init(app);
+
+  module.exports = app;
+}());
