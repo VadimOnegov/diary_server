e23a4fd6f5b682d1a05d6223a08b8540894dd282
Вадим Онегов <vadim.onegov@bars-open.ru>
2025-12-03 18:56:12 +0300

    5

--- config/route-config.js
+++ config/route-config.js
@@ -9,7 +9,7 @@
     const daysRoutes = require('../routes/days');
 
     // *** register routes *** //
-    //app.use('/', routes);
+    app.use('/', routes);
     app.use('/menu', menuRoutes);
     app.use('/days', daysRoutes);
   };
